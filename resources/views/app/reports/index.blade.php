354460c713363d4181b202db2a193e2f6e71bafe
saumya140 <105532306+saumya140@users.noreply.github.com>
2023-05-26 12:46:47 +0800

    report module routing
    -i have a done for navi and also my module view pages

---
+++ resources/views/app/reports/index.blade.php
@@ -0,0 +1,16 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Reports</title>
+</head>
+<body>
+    <h1>Reports</h1>
+    <ul>
+        <li>Sales Report</a></li>
+        <li>Inventory Report</a></li>
+        <li>Orders Report</a></li>
+    </ul>
+</body>
+</html>
