a5d760ae2bb41eb5172b6096588e3eea145594c7
zhikang168 <nzk820@gmail.com>
2023-06-18 23:20:35 +0800

    update dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,6 +13,12 @@
                         Welcome To Petakom Mart
                     </h1>
                     <img src="{{ asset('images/petakom.png') }}" alt="Petakom" class="mx-auto" style="width: 500px; margin-top: -130px;">
+                    <h2 style="font-weight: bold; text-decoration: underline; margin-top: -100px;">Introduction </h2>
+                    <h2>Petakom Mart is a mini market for the student of Faculty Computer. </h2>
+                    <h2>We offer a wide range of quality products and provide excellent customer service. </h2>
+                    <h2>Visit us for a convenient and enjoyable shopping experience.</h2>
+                    
+               
                 </div>
             </div>
         </div>
