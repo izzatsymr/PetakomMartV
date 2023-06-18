52ebe609f267bc5d35626141cd7e16cb9b506c4e
zhikang168 <nzk820@gmail.com>
2023-06-18 23:02:20 +0800

    edit dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-6xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
@@ -8,7 +8,12 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                <div class="relative text-center">
+                    <h1 style="font-size: 50px; margin-bottom: 20px;">
+                        Welcome To Petakom Mart
+                    </h1>
+                    <img src="{{ asset('images/petakom.png') }}" alt="Petakom" class="mx-auto" style="width: 500px; margin-top: -130px;">
+                </div>
             </div>
         </div>
     </div>
